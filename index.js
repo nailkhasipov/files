d56485d79927e8dc081944305f1d2d2fc534fc64
Nail Khasipov <nail.khasipov@gmail.com>
2021-04-09 01:12:43 +0500

    file upload

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const multer  = require('multer')
+
 const app = express()
-const port = 3000
+const port = 3001
+
+var upload = multer({ dest: './uploads/' })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/upload', upload.single('file'), function (req, res) {
+    const fileId = req.file.filename
+    res.send(fileId)
+ });
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`File app listening at http://localhost:${port}`)
 })
